478996fd97e8c23b09feb40dee6975bc1bf72289
Tinu205 <dazleroger404@gmail.com>
2023-07-10 19:54:19 +0530

    Fixed the finding link bug

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
-from main import give_subs
+from main import give_subs,check_link
 import telebot
-import re
+
 
 bot_tok = os.environ.get("BOT_TOKEN")
 bot = telebot.TeleBot(bot_tok)
@@ -10,20 +10,24 @@ bot = telebot.TeleBot(bot_tok)
 def send_welcome(message):
     bot.reply_to(message, "Hello!!, please type `/help` to get help !!")
 
+    print(f"{message.from_user.username}:{message.from_user.first_name} -> {message.text}")
+
 @bot.message_handler(commands=['help'])
 def send_help(message):
     bot.reply_to(message, "Type `/getsubs` then paste the link for your video!! ")
+
+    print(f"{message.from_user.username}:{message.from_user.first_name} -> {message.text}")
 
 @bot.message_handler(commands=['getsubs'], content_types=["text"])
 def f_link(message):
     arr = message.text.strip()  # Remove leading/trailing whitespace
     fulnk = arr.split("getsubs")[1].strip()  # Remove leading/trailing whitespace
+    print(f"{message.from_user.username}:{message.from_user.first_name} -> {message.text}")
     if (fulnk):
         try:
-            video_id = fulnk.split("watch?v=")[1]
-            bot.reply_to(message, give_subs(video_id))
-        except IndexError:
-            bot.reply_to(message,"Please give a valid youtube link")
+            bot.reply_to(message,give_subs(check_link(fulnk)))
+        except:
+            bot.reply_to(message,"### Message form developer the telegram api dosent allows to send too many strings so please bare with until we fix this, you can try with videos with less subs!!!, thank you!!")
     else:
         bot.reply_to(message, "Command usage: /getsubs <link>")
 
